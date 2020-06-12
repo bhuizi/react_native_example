0b826735e27c1446346116a5f7c8af7e98a9707e
bryan huizi <bryan.huizi@magnet360.com>
2020-06-11 20:51:16 -0700

    Adds Home directory Adds base architectural style

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import Home from './src/components/Home';
 
 const Stack = createStackNavigator();
 
@@ -19,7 +12,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
